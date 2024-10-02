09ea4c42c3eaf60ec01d99112091fec2c11b5123
AmabilieMabi <vitoriaamabiliebotazolimacedom@gmail.com>
2024-10-02 17:53:32 +0000

    vivi0210

---
+++ src/main/java/application/repository/PlataformaRepository.java
@@ -0,0 +1,8 @@
+package application.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import application.model.plataforma;
+
+public class PlataformaRepository  extends JpaRepository<Plataforma, Long> {
+    
+}
